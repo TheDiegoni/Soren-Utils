5a14719d2ba263efec05e384eb1105c837de8f33
TheDiegoni <thediegoni575@gmail.com>
2026-04-21 08:28:16 +0200

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -15,10 +15,11 @@ int main(){
 	Color color;
 	cls();
 	cout<<lower("BuOnGiorNO!")<<endl;
-	cout<<"Colonne: "<<termSize(0)<<endl;
-	cout<<"Righe: "<<termSize(1)<<endl;
-	for(int i=0; i<termSize(1); i++){
-		for(int j=0; j<termSize(0); j++){
+  int columns=termSize(0); int rows=termSize(1);
+	cout<<"Colonne: "<<columns<<endl;
+	cout<<"Righe: "<<rows<<endl;
+	for(int i=0; i<rows; i++){
+		for(int j=0; j<columns; j++){
 			cout<<'o';
 		};
 		cout<<endl;
