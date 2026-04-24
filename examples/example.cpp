edd6f25bd0dd698b7e3684cb2fcefd1365b2cb10
TheDiegoni <thediegoni575@gmail.com>
2026-04-24 23:42:15 +0200

    Introduce wait function calls in example.cpp
    Added wait function calls to pause execution.

--- examples/example.cpp
+++ examples/example.cpp
@@ -45,6 +45,7 @@ int main(){
 	alignOut("Ciao, questo e un testo di prova per l'allineamento a destra...dafssssssssssssssssssssssssssssssssssssssssssssswwwwwwwwwwwwwwwwwwwarwerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrfesaaffffffffffffffffww", 0, 10); cout<<endl;
 	alignOut("Ciao, questo e un testo di prova per l'allineamento a centro...dafssssssssssssssssssssssssssssssssssssssssssssswwwwwwwwwwwwwwwwwwwarwerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrfesaaffffffffffffffffww", 1); cout<<endl;
 	alignOut("Ciao, questo e un testo di prova per l'allineamento a sinistra.dafssssssssssssssssssssssssssssssssssssssssssssswwwwwwwwwwwwwwwwwwwarwerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrfesaaffffffffffffffffww", 2, -20); cout<<endl;
+	wait(3, true);
 	string c=alignIn("Inserisci Stringa Sinistra: ", 0, 3);
 	cout<<endl; alignOut(c, 0); cout<<endl;
 	c=alignIn("Inserisci Stringa Destra: ", 2, -3);
@@ -56,6 +57,6 @@ int main(){
 	c=alignIn("Inserisci Stringa Centro: ", 1);
 	cout<<endl; alignOut(c, 0);
 	cout<<color.t_bk<<color.b_gn<<"Ciao "<<color.tb_rd<<color.bb_yw<<"Mondo!"<<color.cl<<" Close";
-	cin.get();
+	wait(5);
 	return 0;
 }
